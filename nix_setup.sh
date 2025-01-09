d1b3e8bd145a03c3b7d59b5b41d72f0707bb54ca
Jack Kolb <jack@jackkolb.com>
2025-01-09 15:56:52 -0500

    updated nix

--- nix_setup.sh
+++ nix_setup.sh
@@ -28,7 +28,7 @@ echo "Checking if the tmm-hri environment exists..."
 if ! mamba env list | grep -q "tmm-hri"
 then
     echo "    The tmm-hri environment does not exist. Creating it now..."
-    mamba create -y -n tmm-hri python=3.11 numpy==1.26.4 matplotlib opencv easydict transformers conda-forge::sam-2
+    mamba create -y -v -v -n tmm-hri python=3.11 numpy==1.26.4 matplotlib opencv easydict transformers conda-forge::sam-2
 else
     echo "    The tmm-hri environment already exists, remove it before running this script."
     exit
