a5beb9964c1265aa4c63d83dff35e3099f12cf21
Jack Kolb <jack@jackkolb.com>
2025-01-09 15:47:19 -0500

    updated nix setup

--- nix_setup.sh
+++ nix_setup.sh
@@ -35,7 +35,7 @@ fi
 
 # install the mamba packages
 echo "Installing the Mamba packages to the tmm-hri environment..."
-mamba run -n -v tmm-hri mamba install numpy==1.26.4 matplotlib opencv easydict transformers conda-forge::sam-2 -y
+mamba run -n tmm-hri mamba install numpy==1.26.4 matplotlib opencv easydict transformers conda-forge::sam-2 -y -v
 
 # install mmpose
 echo "Installing mmpose..."
