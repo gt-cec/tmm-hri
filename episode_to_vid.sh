a33a729b5be26282d14fdb8425f4e7a3deb50594
Jack Kolb <jack@jackkolb.com>
2025-01-13 17:40:57 -0500

    updated sim rearrangement

---
+++ episode_to_vid.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# fill in missing frames
+
+# Directory containing the frames
+FRAMES_DIR="episodes/episode"
+# Change to the frames directory
+cd "$FRAMES_DIR" || exit
+# Get the list of existing frames sorted by their number
+# existing_frames=($(ls frame_*.png | sort -V))
+existing_frames=($(ls *_normal.png | sort -V))
+# Iterate through the existing frames
+for frame in "${existing_frames[@]}"; do
+    # Extract the frame number from the filename
+    frame_number=$(echo "$frame" | grep -oE '[0-9]+')
+    # Check for the next frame
+    next_frame_number=$((frame_number + 1))
+    next_frame="Action_${next_frame_number}_0_normal.png"
+    # If the next frame does not exist, copy the current frame to fill the gap
+    if [[ ! -f "$next_frame" ]]; then
+        echo "Missing $next_frame. Filling it with $frame."
+        cp "$frame" "$next_frame"
+    fi
+done
+echo "Frame filling complete."
+
+# make a movie
+cd ../
+export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libffi.so.7
+ffmpeg -framerate 10 -i episodes/episode/frame_%d.png -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4 -y
--- sim_rearrangement_walkthrough.py
+++ sim_rearrangement_walkthrough.py
@@ -135,24 +135,25 @@ def __sim_action__(action:str, char_ids:list=[0,1], object_ids:list=None, surfac
     sim_failure = False  # flag for the sim failing, requires restart
     if sample_source is not None and object_ids == []:  # if sampling the objects
         object_ids = __sample_objects__(sample_source, num=len(char_ids), min_dist=5)
-    elif sample_source is not None and surface_ids == []:  # if sampling the surfaces
-        surface_ids = __sample_objects__(sample_source, num=len(char_ids))
-    for agent_id in char_ids:
+    for agent_id in reversed(char_ids):
         if object_ids is not None and object_ids[agent_id] is not None:
             script = f"<char{agent_id}> [{action}] <{object_ids[agent_id][1]}> ({object_ids[agent_id][0]})"
-        elif surface_ids is not None and surface_ids[agent_id] is not None:
-            script = f"<char{agent_id}> [{action}] <{surface_ids[agent_id][1]}> ({surface_ids[agent_id][0]})"
-        else:
-            script = f"<char{agent_id}> [{action}]"
-        print("Running script:", script)
-        success, sim_failure = __sim_script__(script, output_folder=output_folder, file_name_prefix=file_name_prefix)
+        while True:
+            print("Running script:", script)
+            success, sim_failure = __sim_script__(script, output_folder=output_folder, file_name_prefix=file_name_prefix)
+            if input("c to continue") == "c":
+                break
         if not success:
             print("FAILURE", sim_failure)
             break
         print("Success!")
     print("Make character 1 look at character 2")
-    script = f"<char0> [lookat] {char_ids[1]}"
-    success, sim_failure = __sim_script__(script, output_folder=output_folder, file_name_prefix=file_name_prefix)
+    script = f"<char0> [lookat] <{object_ids[0][1]}> ({object_ids[0][0]})"
+    while True:
+        print("Running script:", script)
+        success, sim_failure = __sim_script__(script, output_folder=output_folder, file_name_prefix=file_name_prefix)
+        if input("c to continue") == "c":
+            break
     if surface_ids is None and action not in ["grab", "put"]:
         return object_ids, success, sim_failure
     elif object_ids is None and action not in ["grab", "put"]:
