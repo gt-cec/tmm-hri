e033513e1534ed321683b09d8a89a6d571b87103
Jack Kolb <jack@jackkolb.com>
2025-05-28 11:01:02 -0400

    added graphics flag

--- sim_start.py
+++ sim_start.py
@@ -3,12 +3,13 @@
 from virtualhome.virtualhome.simulation.unity_simulator import comm_unity
 
 file_name = "./linux_exec/linux_exec.v2.3.0.x86_64"
-port= "8080"
+port= "5050"
 
 comm = comm_unity.UnityCommunication(
     file_name = file_name,
     port = port,
-    x_display=":0"
+    graphics = False
+    # x_display=":0"
 )
 
 env_id = 0 # env_id ranges from 0 to 6
