cc13f265c1c4de18e33d9b2001254b59c16b98b5
Jack Kolb <jack@jackkolb.com>
2025-05-28 10:22:50 -0400

    trying sim start from script

---
+++ sim_start.py
@@ -0,0 +1,14 @@
+# sim_start.py: launch the simulator
+
+from virtualhome.virtualhome.simulation.unity_simulator import comm_unity
+
+file_name = "./linux_exec/linux_exec.v2.3.0.x86_64"
+port= "8080"
+
+comm = comm_unity.UnityCommunication(
+    file_name = file_name,
+    port = port
+)
+
+env_id = 0 # env_id ranges from 0 to 6
+comm.reset(env_id)
